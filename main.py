16950ef506daac1ab4f0e9d44ab3924357c39140
heyeepc <2675458141@qq.com>
2026-04-09 21:01:27 +0800

    Update main.py

--- main.py
+++ main.py
@@ -27,4 +27,36 @@ class Card():
             FaceNum = 13
         else:
             FaceNum = int(self.rank)
+        if self.suit == "梅":
+            suit=1
+        elif self.suit == "方":
+            suit=2
+        elif self.suit == "红":
+            suit=3
+        else:
+            suit=4
+        return (suit-1)*13+FaceNum
+
+    def flip(self):
+        self.is_face_up = not self.is_face_up
+class Hand():
+    def __init__(self, suit):
+        self.cards = []
+    def __str__(self):
+        if self.cards:
+            rep = ""
+            for card in self.cards:
+                rep = rep + str(card) + "\t"
+                
+            else:
+                rep = "无牌"
+            return rep
+    def clear(self):
+        self.cards = []
+    def add(self,card):
+        self.cards.append(card)
+    def give(self,card,other_hand):
+        self.cards.remove(card)
+        other_hand.add(card)
+
         
