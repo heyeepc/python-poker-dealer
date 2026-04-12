02af3c9795883f23d79a8ff0f23f9fdf2f543bd1
heyeepc <2675458141@qq.com>
2026-04-12 23:12:57 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,103 +1,91 @@
+import random
+
+
 class Card():
+    RANKS = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+    SUITS = ["梅", "方", "红", "黑"]
 
-    RANKS = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"]
-
-    SUITS = ["梅","方","红","黑"]
-
-    def __init__(self, rank, suit,face_up=True):
+    def __init__(self, rank, suit, face_up=True):
         self.rank = rank
         self.suit = suit
         self.is_face_up = face_up
 
     def __str__(self):
         if self.is_face_up:
-            rep = self.suit+self.rank
+            rep = self.suit + self.rank
         else:
             rep = "XX"
         return rep
 
-    def pic_order(self):
-        if self.rank == "A":
-            FaceNum = 1
-        elif self.rank == "J":
-            FaceNum = 11
-        elif self.rank == "Q":
-            FaceNum = 12
-        elif self.rank == "K":
-            FaceNum = 13
-        else:
-            FaceNum = int(self.rank)
-        if self.suit == "梅":
-            suit=1
-        elif self.suit == "方":
-            suit=2
-        elif self.suit == "红":
-            suit=3
-        else:
-            suit=4
-        return (suit-1)*13+FaceNum
-
     def flip(self):
         self.is_face_up = not self.is_face_up
+
+
 class Hand():
-    def __init__(self, suit):
+    def __init__(self):  # 移除了多余的 suit 参数
         self.cards = []
+
     def __str__(self):
         if self.cards:
             rep = ""
             for card in self.cards:
                 rep = rep + str(card) + "\t"
-
         else:
             rep = "无牌"
         return rep
 
     def clear(self):
         self.cards = []
-    def add(self,card):
+
+    def add(self, card):
         self.cards.append(card)
-    def give(self,card,other_hand):
+
+    def give(self, card, other_hand):
         self.cards.remove(card)
         other_hand.add(card)
 
-class poke(Hand):
 
+class Poke(Hand):  # 建议类名首字母大写
     def populate(self):
         for suit in Card.SUITS:
             for rank in Card.RANKS:
-                self.add(Card(rank,suit))
+                self.add(Card(rank, suit))
 
     def shuffle(self):
-
-        import random
         random.shuffle(self.cards)
-        def dea(self,hands,per_hand=13):
-            for rounds in range(per_hand):
-                for hand in hands:
-                    if self.cards:
-                        top_card = self.cards.pop[0]
-                        self.cards.remove(top_card)
-                        hand.add(top_card)
 
+    def deal(self, hands, per_hand=13):  # 修正了缩进和拼写
+        for rounds in range(per_hand):
+            for hand in hands:
+                if self.cards:
+                    top_card = self.cards.pop(0)  # pop 使用圆括号，且会自动删除该元素
+                    hand.add(top_card)
                 else:
                     print("不能继续发牌，牌已经发完")
+                    return  # 牌完即止
 
-if __name__=="__main__":
-    
-    print("this is a module with classes for playing cards")
-    players=[Hand(),Hand(),Hand(),Hand]
-    poke1=poke()
+
+if __name__ == "__main__":
+    print("--- 扑克牌分发系统 ---")
+
+    # 1. 创建4个玩家实例
+    players = [Hand(), Hand(), Hand(), Hand()]
+
+    # 2. 创建一叠牌并初始化
+    poke1 = Poke()
     poke1.populate()
     poke1.shuffle()
-    poke1.deal(players,13)
+
+    # 3. 发牌
+    poke1.deal(players, 13)
+
+    # 4. 显示结果
     n = 1
     for hand in players:
-        print("牌手",n,end=":")
-        print(hand)
-        n+=1
-    input("\npress the enter key to exit")
-    
+        print(f"牌手 {n}: {hand}")
+        n += 1
 
+    input("\n按回车键退出")
 
 
         
